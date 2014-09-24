038652eae67c6094e4f7260fbb011d5ae015f968
IMeyerKimera <meyer@umomail.com>
2014-09-23 22:31:57 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Basic flask app for adding two numbers
--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
+@app.route("/hello")
+def hello():
     return "Hello world!"
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        value_one = int(request.form['number-one'])
+        value_two = int(request.form['number-two'])
+        total = value_one + value_two
+        return render_template('index.html', string='World', value=total)
+    return render_template('index.html', string='World')
 
 if __name__ == '__main__':
     app.run(debug=True)
